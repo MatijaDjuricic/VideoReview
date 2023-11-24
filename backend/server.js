d04804fd9e1009afa10fa26f179ca713d6f3dd51
Matija Djuricic <98923192+MatijaDjuricic@users.noreply.github.com>
2023-11-24 11:21:10 +0100

    initial commit.

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.set('trust proxy', 1);
-app.use(cors());
 app.use(cors({
     origin: "https://videoreview.netlify.app",
     allowedHeaders: "https://videoreview.netlify.app",
