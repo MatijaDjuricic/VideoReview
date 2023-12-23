b23039584da387a9d0d58ed58f87d05bb9f0d0b9
Matija Djuricic <98923192+MatijaDjuricic@users.noreply.github.com>
2023-12-23 23:18:58 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': "https://videoreview.netlify.app"
+      '/api': "https://video-review-app.onrender.com"
     }
   },
   plugins: [react()],
